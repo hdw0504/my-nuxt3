e9cbbc56461fcc6471e3caf0b5dad3839f2e2826
hdw0504 <768685351@qq.com>
2022-11-11 19:27:58 +0800

    auto import vue component and add nuxt/test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,16 @@
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vitest/config'
 
 // https://github.com/Qiskit/saiba/blob/ed74ec7471c8ae481928ffd22d8e9f3bced02ed5/vitest.config.ts
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
+    deps: {
+      inline: ['@nuxt/test-utils-edge'],
+    },
   },
   resolve: {
     alias: {
@@ -17,6 +22,9 @@ export default defineConfig({
       imports: ['vue', '@vueuse/core'],
       vueTemplate: true,
       dts: true,
+    }),
+    Components({
+      dirs: ['./components'],
     }),
   ],
 })
