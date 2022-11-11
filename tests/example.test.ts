e9cb891d0a4f50f3c34c33729f1fd7fe981fca7d
hdw0504 <768685351@qq.com>
2022-11-11 18:50:54 +0800

    add browser test

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,9 +1,5 @@
-import { add } from '@/composables/utils'
+import { add } from '@/composables'
 
-// it('test', () => {
-//   expect(filterRef([1, 2, 3, 4], a => a !== 1)).toMatchInlineSnapshot('"foobar"')
-// })
-
-it('test', () => {
+it('test add', () => {
   expect(add(ref(1), ref(4))).toMatchInlineSnapshot('5')
 })
