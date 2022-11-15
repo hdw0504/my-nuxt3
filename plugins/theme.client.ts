8a8f0e51b2800c98f229975dd10fdd5796d45014
hdw0504 <768685351@qq.com>
2022-11-16 01:32:43 +0800

    add log

--- plugins/theme.client.ts
+++ plugins/theme.client.ts
@@ -5,8 +5,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   // SSR is causing dark mode hydration issues with naive-ui
   // This fix quickly toggles dark mode on 'page:finish' hook
   nuxtApp.hook('page:finish', () => {
+    // First time on the page
     isDark.value = color.value === 'dark'
 
+    // When switch theme
     watch(() => color.value, (val) => {
       isDark.value = val === 'dark'
     }, { immediate: false })
