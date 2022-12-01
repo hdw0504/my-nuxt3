c911216481f8056c8b6e199844873cac013daa21
hdw0504 <768685351@qq.com>
2022-12-02 01:19:37 +0800

    chore: removed unuse files

--- composables/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './count'
-export * from './user'
-export * from './utils'
--- tests/example.test.ts
+++
@@ -1,5 +0,0 @@
-import { add } from '@/composables'
-
-it('test add', () => {
-  expect(add(ref(1), ref(4))).toMatchInlineSnapshot('5')
-})
