3b9b037d48ba1e7930ce7495f5115f145b10731f
hdw <768685351@qq.com>
2023-02-10 09:37:08 +0800

    fix: update

--- scripts/fixPopper.ts
+++ scripts/fixPopper.ts
@@ -1,15 +1,16 @@
 import { copy } from 'fs-extra'
 
 const dereference = process.platform === 'win32' ? true : undefined
+// 部署到netlify是不同路径
+const rootModulePath = process.platform === 'win32' ? '.output' : '.netlify/functions-internal'
 
 ;(async () => {
   // 解决 popperjs 默认是 cjs 导致报错问题
   // https://github.com/element-plus/element-plus/issues/10979
-  console.log('dereference', dereference, process.platform)
 
   await copy(
-    '.netlify/functions-internal/server/node_modules/@sxzz/popperjs-es',
-    '.netlify/functions-internal/server/node_modules/@popperjs/core',
+    `${rootModulePath}/server/node_modules/@sxzz/popperjs-es`,
+    `${rootModulePath}/server/node_modules/@popperjs/core`,
     { dereference },
   )
   console.log('finish cover @popperjs/core from @sxzz/popperjs-es ...')
@@ -17,7 +18,7 @@ const dereference = process.platform === 'win32' ? true : undefined
   // 因为那边需要index.js ...
   await copy(
     'node_modules/@popperjs/core/dist/index.js',
-    '.netlify/functions-internal/server/node_modules/@popperjs/core/dist/index.js',
+    `${rootModulePath}/server/node_modules/@popperjs/core/dist/index.js`,
     { dereference },
   )
   console.log('finish cover index.js ...')
