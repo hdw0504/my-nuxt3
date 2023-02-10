4d7844d9251774224c224af60fc522ddce9814a9
hdw <768685351@qq.com>
2023-02-10 09:21:02 +0800

    chore:test

--- scripts/fixPopper.ts
+++ scripts/fixPopper.ts
@@ -5,6 +5,7 @@ const dereference = process.platform === 'win32' ? true : undefined
 ;(async () => {
   // 解决 popperjs 默认是 cjs 导致报错问题
   // https://github.com/element-plus/element-plus/issues/10979
+  console.log('dereference', dereference, process.platform)
 
   await copy(
     '.netlify/functions-internal/server/node_modules/@sxzz/popperjs-es',
